4aed42c32d803fae5cd5d1e0540561caa52cd30a
geonKO <kws07042@gmail.com>
2025-01-28 17:17:56 +0900

    feat: 뉴스 데이터 관련 타입 정의

---
+++ src/features/news/types/NewsDetail.ts
@@ -0,0 +1,25 @@
+
+
+export interface FeaturedNews {
+    imageUrl: string | undefined;
+    id: string;
+    title: string;
+    content: string;
+    author: string;
+    publishedDate: string;
+}
+
+export interface NewsList {
+    imageUrl: string | undefined;
+    id: string;
+    title: string;
+    subtitle: string;
+    content: string;
+    author: string;
+    publishedDate: string;
+}
+
+export interface NewsData {
+    featured: FeaturedNews[];
+    newsList: NewsList[];
+}
