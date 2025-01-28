72cdf1195b4c01a5b7e05e8cb91979b9ce7e0055
JangHwan <wkd5878@gmail.com>
2025-01-28 21:49:23 +0900

    :sparkle: feat - Footer.tsx 컴포넌트 생성

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import Navigation from "./components/Navigation.tsx";
+import Footer from "./components/Footer.tsx";
 
 function App() {
   const [count, setCount] = useState(0)
@@ -29,6 +30,7 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+      <Footer/>
     </>
   )
 }
