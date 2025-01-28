75c616f33e9476617000fac6a4fd19bfdbf50af7
geonKO <kws07042@gmail.com>
2025-01-28 17:30:13 +0900

    feat: JSON 에서 가져온 뉴스 리스트를 표시하는 NewsList 컴포넌트 구현

---
+++ src/features/news/components/NewsList.tsx
@@ -0,0 +1,26 @@
+// src/features/news/components/NewsList.tsx
+import React from "react";
+import { getNewsData } from "../service/newsService";
+import type { NewsList } from "../types/NewsDetail";
+
+const NewsList: React.FC = () => {
+    const { newsList } = getNewsData();
+    return (
+        <div className="news-list">
+            {newsList.map((news: NewsList) => (
+                <div key={news.id} className="news-item">
+                    <img
+                        src={news.imageUrl}
+                        alt={news.title}
+                        className="featured-image"
+                    />
+                    <h3>{news.title}</h3>
+                    <span>{news.author} | {new Date(news.publishedDate).toLocaleDateString()}</span>
+                    <p>{news.content}</p>
+                </div>
+            ))}
+        </div>
+    );
+};
+
+export default NewsList;
