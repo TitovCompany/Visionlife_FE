573e0c6c5c02a0a2d14557019643f5dc776de2db
JangHwan <wkd5878@gmail.com>
2025-01-29 18:36:30 +0900

    :sparkle: feat - Home 페이지 컴포넌트 About 섹션 마크업 및 디자인

--- src/layout/SectionLayout.tsx
+++ src/layout/SectionLayout.tsx
@@ -1,8 +1,15 @@
 import * as React from "react";
 
-const SectionLayout = ({ children }: { children: React.ReactNode }) => {
+const SectionLayout = ({
+  children,
+  title
+}: {
+  children: React.ReactNode,
+  title: string
+}) => {
   return (
-      <section className="h-screen">
+      <section className="w-full h-screen">
+        <h2 className="w-full p-18 text-center text-6xl font-extrabold">{title}</h2>
         {children}
       </section>
   );
