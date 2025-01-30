651d59bd7c75fa528757d801b2407790017110f8
japark <wkd5878@gmail.com>
2025-01-30 12:22:01 +0900

    :sparkle: feat - 회사 비젼 아이템 컴포넌트 생성 및 비젼 내부 컴포넌트화
    - VisionItem 컴포넌트 생성 (title 별 다른 아이콘 출력)
    - CompanyVisionSection 컴포넌트 생성 (회사 비젼 View 로, Home, Profile 내 적용)

--- src/features/home/index.tsx
+++ src/features/home/index.tsx
@@ -3,6 +3,7 @@ import PageLayout from "../../layout/PageLayout.tsx";
 import HeroSection from "./components/HeroSection.tsx";
 import {Link} from "react-router-dom";
 import NewsCarousel from "./components/NewsCarousel.tsx";
+import CompanyVisionSection from "../../components/CompanyVisionSection.tsx";
 
 const Home = () => {
   return (
@@ -26,19 +27,7 @@ const Home = () => {
 
         {/* Vision */}
         <SectionLayout title="Vision Life We Do" className="flex flex-col justify-center items-center">
-          <p className="text-center text-lg">
-            열악한 환경에 처한 나라나 개발이 필요한 도상국들의 경제활동에 도움을 주는<br/>
-            친환경 기술의 보급을 통하여 균형적인 경제력으로
-          </p>
-          <p className="my-10 text-center text-lg">
-            더불어 함께 행복한 인간 삶을 누릴수 있도록<br/>
-            공헌하는데 그 사명이 있는 회사입니다.
-          </p>
-          <ul className="flex justify-between">
-            <li>친환경 기술</li>
-            <li>경제력 상승</li>
-            <li>함께사는 사회</li>
-          </ul>
+          <CompanyVisionSection/>
         </SectionLayout>
 
         {/* News */}
