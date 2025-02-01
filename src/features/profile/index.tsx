4055e18f07839104dcfcb22db60c1d4efa110228
japark <wkd5878@gmail.com>
2025-02-01 18:42:27 +0900

    :sparkle: feat - 기업 소개 내 어바웃 섹션 작업

--- src/features/profile/index.tsx
+++ src/features/profile/index.tsx
@@ -8,11 +8,34 @@ const Profile = () => {
       <PageLayout title="서비스 페이지 본문">
           {/* About */}
         <div className="mt-20 mx-20">
-          <section>
-            <h2>{profile.title}</h2>
-            {profile.content.map((item, index) => (
-                <p key={index}>{item}</p>
-            ))}
+          <h3 className="hidden">회사 소개</h3>
+          <section className="w-full h-screen max-w-7xl mx-auto">
+            <div className="mt-16 relative">
+              <div className="w-1/2 h-full p-10 bg-[#5c504b] text-white absolute right-0">
+                <p className="mb-3 text-2xl font-bold text-white">
+                  (주)비젼라이프홀딩스는<br/>
+                  친환경 녹색 산업혁명의<br/>
+                  회사입니다.
+                </p>
+                <p className="text-md">
+                  세계유일의 무폐수 나일론 디지털 나염을 상용화에<br/>
+                  성공한 회사로써 전사지를 사용하지 않고 또한<br/>
+                  잉크를 4색(C.M.Y.K) 만으로 실사를 구현하는<br/>
+                  기술을 보유한 친환경 녹색산업혁명의 회사입니다.
+                </p>
+              </div>
+              <img
+                  className="w-full max-h-[350px] pt-12 "
+                  src="/img/home/p2.jpg"
+                  alt="기업 소개 페이지 히어로 이미지"
+              />
+            </div>
+            <article className="mt-10">
+              <h2 className="mb-2 text-xl font-bold">{profile.title}</h2>
+              {profile.content.map((item, index) => (
+                  <p key={index}>{item}</p>
+              ))}
+            </article>
           </section>
 
           {/* Vision */}
@@ -21,8 +44,8 @@ const Profile = () => {
           </SectionLayout>
 
           {/* History */}
-          <section>
-            <h3 className="hidden">기업 히스토리</h3>
+          <section className="w-full h-screen max-w-7xl mx-auto">
+            <h2 className="hidden">기업 히스토리</h2>
           </section>
         </div>
       </PageLayout>
