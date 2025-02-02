72428cbc94500145eff1e8e07456391d8c8dc67d
geonKO <kws07042@gmail.com>
2025-02-02 19:17:22 +0900

    feat: Catalog 페이지에 Catalogcomponent 추가 - Catalogcomponent 컴포넌트를 추가하여 기능 구현

--- src/features/catalog/index.tsx
+++ src/features/catalog/index.tsx
@@ -1,19 +1,14 @@
 import PageLayout from "../../layout/PageLayout.tsx";
+import Catalogcomponent from "./components/Catalog";
 
 const Catalog = () => {
   return (
-<<<<<<< HEAD
-      <div>
-
-      </div>
-  );
-=======
       <PageLayout title="카탈로그 페이지 본문">
         <div className="mx-20">
+            <Catalogcomponent />
         </div>
       </PageLayout>
 );
->>>>>>> upstream/main
 };
 
 export default Catalog;
