a7f2f06cdff62ed5341573c3376e40bd48134ba1
JangHwan <wkd5878@gmail.com>
2025-02-04 00:05:12 +0900

    :bug: fix - vite.config.ts 내 base 경로 수정

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "Visionlife_FE",
+  base: "",
   plugins: [
     react(),
     tailwindcss(),
