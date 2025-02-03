54d5dd16dd6126b985911d16cbb6c31d47d76e7b
JangHwan <wkd5878@gmail.com>
2025-02-04 00:08:57 +0900

    :bug: fix - vite.config.ts 내 base 경로 수정, HashRouter 추가

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,13 @@
 import './index.css'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import {RouterProvider} from "react-router-dom";
+import {HashRouter, RouterProvider} from "react-router-dom";
 import {router} from "./routes.ts";
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <RouterProvider router={router} />
+    <HashRouter>
+      <RouterProvider router={router} />
+    </HashRouter>
   </StrictMode>,
 )
