55866d58152f8f8dbeffbe5df84cf112c8afdb1d
geonKO <kws07042@gmail.com>
2025-02-06 02:20:00 +0900

    feat: 푸터(Footer) 디자인 및 링크 추가
    - 배경색 추가 (`bg-[#E4F2ED]`)
    - 개인정보 취급방침, 이용약관, 이메일 무단 수집 거부 페이지 링크 추가

--- src/layout/Footer.tsx
+++ src/layout/Footer.tsx
@@ -1,8 +1,21 @@
+import { Link } from "react-router-dom";
+
 const Footer = () => {
   return (
-    <footer className="p-4">
+    <footer className="p-4 bg-[#E4F2ED]">
       <section className="mx-auto text-center">
-        <small>Copyright ⓒ visionlife. All Rights Reserved.</small>
+        <small>
+          Copyright ⓒ visionlife. All Rights Reserved.
+          <Link to="/privacy-policy" className="ml-4">
+            개인정보 취급방침
+          </Link>
+          <Link to="/TermsOfService" className="ml-4">
+            이용약관
+          </Link>
+          <Link to="/EmailNoCollection" className="ml-4">
+            이메일무단수집거부
+          </Link>
+        </small>
       </section>
     </footer>
   );
