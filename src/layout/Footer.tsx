4388a68ee5709e5dad6d25564eb2dd818bf3bbf9
geonKO <kws07042@gmail.com>
2025-02-09 18:15:28 +0900

    -refactor 푸터 구조 및 디자인 변경

--- src/layout/Footer.tsx
+++ src/layout/Footer.tsx
@@ -1,24 +1,27 @@
-import { Link } from "react-router-dom";
+  import { Link } from "react-router-dom";
 
-const Footer = () => {
-  return (
-    <footer className="p-4 bg-[#E4F2ED]">
-      <section className="mx-auto text-center">
-        <small>
-          Copyright ⓒ visionlife. All Rights Reserved.
-          <Link to="/privacy-policy" className="ml-4">
-            개인정보 취급방침
-          </Link>
-          <Link to="/TermsOfService" className="ml-4">
-            이용약관
-          </Link>
-          <Link to="/EmailNoCollection" className="ml-4">
-            이메일무단수집거부
-          </Link>
-        </small>
-      </section>
-    </footer>
-  );
-};
+  const Footer = () => {
+    return (
+      <footer className="p-6 bg-[#3EAE48] text-white font-semibold">
+        <div className="max-w-6xl mx-auto flex flex-col md:flex-row justify-between items-center">
+          <div className="text-center md:text-left">
+            <p className="text-sm">© 2025 VisionLife. All Rights Reserved.</p>
+          </div>
+          <small className="flex flex-wrap justify-center gap-4 my-4 md:my-0">
+            <Link to="/privacy-policy" className="text-sm">
+              개인정보 취급방침
+            </Link>
+            <Link to="/TermsOfService" className="text-sm">
+              이용약관
+            </Link>
+            <Link to="/EmailNoCollection" className="text-sm ">
+              이메일무단수집거부
+            </Link>
+          </small>
 
-export default Footer;
+        </div>
+      </footer>
+    );
+  };
+
+  export default Footer;
