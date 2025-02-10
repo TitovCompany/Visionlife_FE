3c20f444ad987e3e3ebd42eb773b3ec4f1005f57
JangHwan <wkd5878@gmail.com>
2025-02-10 16:32:39 +0900

    :sparkle: feat - 컴포넌트 추가
    - Vision Life 소개 섹션을 위한 SectionAbout_Design2.tsx 추가
    - 회사 연혁, 소개, 비즈니스, 오시는 길 리스트 포함
    - 이미지 배경 및 호버 효과 적용

--- src/features/home/components/SectionAbout_Design2.tsx
+++ src/features/home/components/SectionAbout_Design2.tsx
@@ -1,8 +1,13 @@
-import DummyImgBox from '../../../components/DummyImgBox.tsx';
+const serviceItem = [
+  {id: 1, src: "/img/service/p1.webp", title: "회사 연혁"},
+  {id: 2, src: "/img/service/p2.webp", title: "회사 소개"},
+  {id: 3, src: "/img/service/p3.webp", title: "비즈니스"},
+  {id: 4, src: "/img/service/p4.webp", title: "오시는 길"},
+]
 
 const SectionAbout_Design2 = () => {
   return (
-    <section className="pb-20 flex">
+    <section className="flex">
       <article className="w-1/2 px-16 pt-16 pb-80 text-center bg-primary text-white">
         <h2 className="mb-10 text-4xl text-left font-semibold">Vision Life</h2>
         <p className="text-xl text-left leading-8">
@@ -12,18 +17,25 @@ const SectionAbout_Design2 = () => {
         </p>
       </article>
       <ul className="w-full mx-auto flex flex-col">
-        <li className="w-full h-full">
-          <DummyImgBox width="w-full" height="h-full" isCircle={false} />
+        {serviceItem.map(item => (
+          <li key={item.id} className="relative w-full h-32 group overflow-hidden transition-all duration-300 ease-in-out hover:h-64">
+            <h3 className="ml-24 text-3xl font-semibold relative z-10">{item.title}</h3>
+            <img src={item.src} alt={`${item.title} 백그라운드 이미지`} className="absolute inset-0 w-full h-full object-cover blur-md transition-all duration-300 ease-in-out group-hover:h-full"
+            />
+          </li>
+        ))}
+        {/*<li className="w-full h-full flex justify-start items-center">
+          <h3 className="ml-32 text-2xl font-semibold">회사 연혁</h3>
         </li>
-        <li className="w-full h-full">
-          <DummyImgBox width="w-full" height="h-full" isCircle={false} />
+        <li className="w-full h-full flex justify-start items-center">
+          <h3 className="ml-32 text-2xl font-semibold">회사 소개</h3>
         </li>
-        <li className="w-full h-full">
-          <DummyImgBox width="w-full" height="h-full" isCircle={false} />
+        <li className="w-full h-full flex justify-start items-center">
+          <h3 className="ml-32 text-2xl font-semibold">비즈니스</h3>
         </li>
-        <li className="w-full h-full">
-          <DummyImgBox width="w-full" height="h-full" isCircle={false} />
-        </li>
+        <li className="w-full h-full flex justify-start items-center">
+          <h3 className="ml-32 text-2xl font-semibold">오시는 길</h3>
+        </li>*/}
       </ul>
     </section>
   );
--- src/features/home/index.tsx
+++ src/features/home/index.tsx
@@ -1,6 +1,6 @@
 import imageSrc from '/img/banner/p1.jpg';
 import PageLayout from '../../layout/PageLayout.tsx';
-import ProductList from './components/ProductList.tsx';
+// import ProductList from './components/ProductList.tsx';
 import NewsCard from './components/NewsCard.tsx';
 import MoreNewsSection from './components/MoreNewsSection.tsx';
 import {AiOutlineArrowDown} from 'react-icons/ai';
@@ -10,13 +10,15 @@ import SectionAbout_Design2 from './components/SectionAbout_Design2.tsx';
 const Home = () => {
   return (
     <PageLayout title="서비스 페이지 본문">
-      <div className="h-full w-full">
-        <HeroSection src={imageSrc} sectionClass="h-[410px] md:h-[600px] grid grid-cols-1 grid-rows-1"/>
+      <div className="w-full h-screen">
+        <HeroSection
+          src={imageSrc}
+          sectionClass="h-[410px] md:h-[635px]  grid grid-cols-1 grid-rows-1"/>
         {/*<SectionAbout_Design1/>*/}
         <SectionAbout_Design2/>
-
+      </div>
         {/* 제품 */}
-        <section className="mx-auto max-w-4xl">
+        {/*<section className="mx-auto max-w-4xl">
           <ProductList
             title="고객과 함께 하는 친환경 녹색 미래"
             description="비젼라이프는 지속 가능한 미래를 위해 친환경 기술을 연구하며, 혁신적인 솔루션으로 녹색 산업을 선도합니다."
@@ -27,10 +29,10 @@ const Home = () => {
             description="UTEX는 전·후처리 공정 없이 깨끗한 염색을 실현하는 혁신적인 기술로, 친환경 섬유 산업의 새로운 기준을 만들어갑니다."
             type="_right"
           />
-        </section>
+        </section>*/}
 
         {/* 뉴스 */}
-        <section className="mx-32 h-96 py-20">
+        <section className="pt-20 mx-32 h-96 py-20">
           <h2 className="mb-3 ml-3 flex items-center gap-3 text-lg font-semibold">
             <AiOutlineArrowDown className="text-primary font-extrabold" />
             비전라이프 최신 소식
@@ -46,7 +48,6 @@ const Home = () => {
         <section className="mx-auto h-96 max-w-3xl py-20">
           <MoreNewsSection />
         </section>
-      </div>
     </PageLayout>
   );
 };
