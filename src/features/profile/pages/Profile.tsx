e1daabe6456cbdf0e80c7414dcb13041b445553c
JangHwan <wkd5878@gmail.com>
2025-02-11 12:52:57 +0900

    :recycle: refactor - Profile.tsx 내 ContactUs 섹션 분리

--- src/features/profile/pages/Profile.tsx
+++ src/features/profile/pages/Profile.tsx
@@ -2,25 +2,22 @@ import imageSrc from '/img/home/p2.jpg';
 import HeroSection from '../components/HeroSection.tsx';
 import SectionLayout from '../../../layout/SectionLayout.tsx';
 import ListCard from '../../../components/ListCard.tsx';
-import {MdEco, MdOutlineMail} from 'react-icons/md';
-import {FaChartLine, FaHandsHelping, FaPhoneAlt} from 'react-icons/fa';
-import {LiaFaxSolid} from 'react-icons/lia';
-import {HiOutlineLocationMarker} from 'react-icons/hi';
-import DummyImgBox from '../../../components/DummyImgBox.tsx';
+import {MdEco} from 'react-icons/md';
+import {FaChartLine, FaHandsHelping} from 'react-icons/fa';
 import PageLayout from '../../../layout/PageLayout.tsx';
 
 const Profile = () => {
   return (
     <PageLayout title="서비스 페이지 본문">
       {/* Hero */}
-      <HeroSection src={imageSrc} sectionClass="h-screen"/>
+      <HeroSection src={imageSrc} sectionClass="h-[410px] md:h-[635px]"/>
 
       {/* About */}
       <SectionLayout
         title="주식회사 비젼 라이프 홀딩스 회사 소개"
         className="mx-auto flex max-w-4xl flex-col items-center justify-center"
         titleClassName="hidden"
-        fullHeight={true}>
+        fullHeight={false}>
         <article>
           <h3 className="text-primary p-10 text-center text-3xl font-semibold">
             ECO FRIENDLY
@@ -44,7 +41,7 @@ const Profile = () => {
         title="주식회사 비젼 라이프 홀딩스 목표 및 방향성"
         className="mx-auto flex max-w-4xl flex-col items-center justify-center"
         titleClassName="hidden"
-        fullHeight={true}>
+        fullHeight={false}>
         <ul className="flex flex-col gap-3">
           <ListCard
             title="친환경 기술"
@@ -68,65 +65,6 @@ const Profile = () => {
             icon={<FaHandsHelping className="text-3xl text-orange-500" />}
           />
         </ul>
-      </SectionLayout>
-
-      {/* Contact Us */}
-      <SectionLayout
-        title="CONTACT US"
-        className="mx-auto flex max-w-6xl flex-col items-center justify-center"
-        titleClassName="text-6xl font-normal"
-        fullHeight={true}>
-        <p className="mb-16 max-w-2xl text-center text-lg leading-relaxed text-gray-700">
-          궁금한 점이 있으신가요? 언제든지 문의해 주세요.
-          <br />
-          비젼라이프홀딩스는 고객과의 소통을 중요하게 생각하며,
-          <br />
-          빠르고 친절한 응대를 약속드립니다.
-        </p>
-
-        <div className="grid h-[600px] w-full grid-cols-5 grid-rows-4 gap-x-2">
-          <ul className="col-span-2 row-span-4 grid grid-rows-4 gap-y-1">
-            <ListCard
-              title="전화번호"
-              type="_base"
-              icon={
-                <FaPhoneAlt className="bg-primary h-14 w-14 rounded-full p-3 text-white" />
-              }
-              description="02-555-1202"
-              size="w-full h-full"
-            />
-            <ListCard
-              title="팩스 (FAX)"
-              type="_base"
-              icon={
-                <LiaFaxSolid className="bg-primary h-14 w-14 rounded-full p-3 text-white" />
-              }
-              description="02-555-5801"
-              size="w-full h-full"
-            />
-            <ListCard
-              title="이메일"
-              type="_base"
-              icon={
-                <MdOutlineMail className="bg-primary h-14 w-14 rounded-full p-3 text-white" />
-              }
-              description="visionlife2018@naver.com"
-              size="w-full h-full"
-            />
-            <ListCard
-              title="주소"
-              type="_base"
-              icon={
-                <HiOutlineLocationMarker className="bg-primary h-14 w-16 rounded-full p-3 text-white" />
-              }
-              description="서울 서초구 반포대로 21길 23 세진빌딩 3층"
-              size="w-full h-full"
-            />
-          </ul>
-          <div className="col-span-3 row-span-4 grid-cols-2">
-            <DummyImgBox width="w-full" height="h-full" isCircle={false} />
-          </div>
-        </div>
       </SectionLayout>
     </PageLayout>
   );
