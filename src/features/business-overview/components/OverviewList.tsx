e23ceb1a1e7367c139c309e972654b10273c79e4
geonKO <kws07042@gmail.com>
2025-02-12 18:55:14 +0900

    refactor: OverviewList.tsx 개선 및 아이콘 추가
    - 리스트 아이템에 아이콘 추가
    - max-w-4xl로 변경하여 넓게 확장
    - 리스트 아이에 flex items-center gap-3 추가하여 간격 정렬 조정

--- src/features/business-overview/components/OverviewList.tsx
+++ src/features/business-overview/components/OverviewList.tsx
@@ -1,14 +1,19 @@
-import {ReactNode} from 'react';
+import { ReactNode } from "react";
+import { AiOutlineCheckCircle } from "react-icons/ai"; // 체크 아이콘
 
 interface Props<T> {
   data: T[];
 }
 
-const OverviewList = <T extends ReactNode>({data}: Props<T>) => {
+const OverviewList = <T extends ReactNode>({ data }: Props<T>) => {
   return (
-    <ul className="mx-auto w-full max-w-2xl rounded-lg border border-gray-300 bg-white p-6 shadow-md">
+    <ul className="mx-auto w-full max-w-4xl rounded-lg border border-gray-300 bg-white p-6 shadow-md">
       {data.map((item, index) => (
-        <li key={index} className="text-[1rem] leading-8">
+        <li
+          key={index}
+          className="flex items-center gap-3 text-[1rem] leading-8 font-medium text-gray-800"
+        >
+          <AiOutlineCheckCircle className="text-green-500" size={20} />
           <p>{item}</p>
         </li>
       ))}
