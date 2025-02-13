82faed2a2340ad58d8971efbb7edaba73af0572a
JangHwan <wkd5878@gmail.com>
2025-02-13 11:26:51 +0900

    :art: design - 히스토리 디자인 변경

--- src/features/profile/pages/History.keeps.tsx
+++ src/features/profile/pages/History.keeps.tsx
@@ -5,7 +5,7 @@ import history from '../../../data/history.json';
 
 gsap.registerPlugin(ScrollTrigger); // GSAP 플러그인 등록
 
-const History = () => {
+const HistoryKeeps = () => {
   const sectionRef = useRef(null);
 
   useEffect(() => {
@@ -29,20 +29,21 @@ const History = () => {
   }, []);
 
   return (
-    <section ref={sectionRef} className="mx-auto min-h-screen w-full max-w-5xl py-32">
-      <div className="mb-20 font-bold">
-        <h2 className="mb-10 text-4xl">
-          <span className="text-primary">비젼라이프</span>가 걸어온 길</h2>
-        <h3 className="text-3xl">
-          지속 가능한 세상을 만들기 위한<br/>
-          비젼라이프의 노력과 성취를 돌아봅니다.
-        </h3>
-      </div>
+    <section ref={sectionRef} className="mx-auto min-h-screen w-full max-w-7xl py-32">
+      <h2 className="mb-10 text-center text-3xl font-bold">비젼라이프가 걸어온 길</h2>
+      <h3 className="mb-10 text-center text-3xl font-bold">지속 가능한 세상을 만들기 위한 비젼라이프의 노력과 성취를 돌아봅니다.</h3>
       <ul className="flex flex-col space-y-12">
-        {history.map(item => (
-          <li key={item.id} className="history_item flex justify-start items-center">
-            <h4 className="pr-5 text-2xl text-right font-semibold">{item.year}년</h4>
-            <div className="text-base">{item.event}</div>
+        {history.map((item, idx) => (
+          <li key={item.id} className="history_item flex items-center">
+            <div
+              className={`w-1/4 pr-5 text-right font-semibold text-gray-600 ${idx % 2 === 0 ? 'order-1' : 'order-2'}`}>
+              {item.year}
+            </div>
+            <div
+              className={`w-3/4 rounded-lg bg-gray-100 p-5 shadow-lg ${idx % 2 === 0 ? 'order-2' : 'order-1'}`}>
+              {' '}
+              {item.event}
+            </div>
           </li>
         ))}
       </ul>
@@ -50,4 +51,4 @@ const History = () => {
   );
 };
 
-export default History;
+export default HistoryKeeps;
