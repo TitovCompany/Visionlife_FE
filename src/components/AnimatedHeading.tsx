b44ea595fd0e804b35facabe9f800848b54f86af
JangHwan <wkd5878@gmail.com>
2025-02-13 10:12:39 +0900

    :recycle: refactor - 컴포넌트로 분리
    - AnimatedHeading.tsx 컴포넌트 생성 및 적용

---
+++ src/components/AnimatedHeading.tsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import clsx from "clsx";
+
+interface HoverUnderlineTitleProps {
+  as?: "h1" | "h2" | "h3" | "h4" | "h5" | "h6" | "p";
+  title: string;
+  className?: string;
+  underlineColor?: string;
+  hoverEffect?: boolean;
+  ariaLevel?: number;
+  role?: "heading" | "presentation" | "none";
+}
+
+const AnimatedHeading: React.FC<HoverUnderlineTitleProps> = (props) => {
+  const {
+    as: Tag = 'h3',
+    title,
+    className = "",
+    underlineColor = "bg-primary",
+    hoverEffect = true,
+    ariaLevel,
+    role = "heading",
+  } = props;
+
+  const classes = clsx(
+    'relative inline-block font-semibold text-gray-800',
+    className);
+
+  const borderClasses = clsx(
+    'absolute bottom-0 left-0 h-0.5 w-0 transition-all duration-500',
+    hoverEffect && 'group-hover:w-full',
+    underlineColor);
+
+  return (
+    <Tag className={classes}
+      role={role}
+      {...(ariaLevel ? { 'aria-level': ariaLevel } : {})}>
+      {title}
+      <span className={borderClasses}></span>
+    </Tag>
+  );
+};
+
+export default AnimatedHeading;
