31548068d6e044063600e01fcbf801fb0218992b
JangHwan <wkd5878@gmail.com>
2025-02-15 20:41:31 +0900

    :recycle: refactor - Footer 컴포넌트 리팩토링 및 캐싱
    - FooterColumn.tsx 컴포넌트 생성 (Footer Item 공통 컴포넌트)
    - memo() 적용하여 불필요한 리렌더링 방지
    - useMemo()로 footer 데이터 캐싱하여 성능 개선
    
    - RootLayout.tsx 내 container max-w-[1440px] mx-auto 클래스를 제거하여 불필요한 여백 제거 (연관 이슈 : https://github.com/TitovCompany/Visionlife_FE/issues/32)

--- src/layout/RootLayout.tsx
+++ src/layout/RootLayout.tsx
@@ -1,11 +1,11 @@
 import '../index.css';
 import Navigation from './Navigation.tsx';
-import Footer from './Footer.tsx';
+import Footer from './Footer';
 import {Outlet} from 'react-router-dom';
-
+// container max-w-[1440px] mx-auto
 function RootLayout() {
   return (
-    <div className="container mx-auto h-full w-full max-w-[1440px]">
+    <div className="w-full h-full">
       <Navigation />
       <Outlet />
       <Footer />
