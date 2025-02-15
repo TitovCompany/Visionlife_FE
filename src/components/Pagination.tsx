970f164fb34cb48893a73a7d79b22146f4e649a6
ko <kws07042@gmail.com>
2025-02-16 05:12:39 +0900

    refactor: 페이지네이션 스타일  개선
    - gap 값을 sm:gap-4 md:gap-2로 조정하여 화면 크기에 따른 간격 최적화

--- src/components/Pagination.tsx
+++ src/components/Pagination.tsx
@@ -14,9 +14,9 @@ const Pagination: React.FC<PaginationProps> = ({
   if (totalPages <= 1) return null;
 
   return (
-    <div className="mt-6 flex flex-wrap justify-center items-center gap-2 sm:gap-4">
+    <div className="mt-6 flex flex-wrap justify-center items-center gap-2 sm:gap-4 md:gap-2">
       <button
-        className={`px-4 sm:px-8 py-1 text-base sm:text-lg ${
+        className={`px-3 sm:px-6 md:px-8 py-1 sm:py-2 text-sm sm:text-base md:text-lg ${
           currentPage === 1 ? 'cursor-not-allowed opacity-50' : 'hover:underline'
         }`}
         onClick={() => currentPage > 1 && onPageChange(currentPage - 1)}
@@ -31,7 +31,7 @@ const Pagination: React.FC<PaginationProps> = ({
         return (
           <button
             key={pageNumber}
-            className={`px-3 sm:px-6 py-1 text-base sm:text-lg transition-colors rounded ${
+            className={`px-2 sm:px-3 md:px-4 py-1 sm:py-2 text-sm sm:text-base md:text-lg transition-colors rounded ${
               currentPage === pageNumber
                 ? 'font-bold !text-primary'
                 : 'text-gray-700 hover:text-primary'
@@ -44,8 +44,10 @@ const Pagination: React.FC<PaginationProps> = ({
       })}
 
       <button
-        className={`px-4 sm:px-8 py-1 text-base sm:text-lg ${
-          currentPage === totalPages ? 'cursor-not-allowed opacity-50' : 'hover:underline'
+        className={`px-3 sm:px-6 md:px-8 py-1 sm:py-2 text-sm sm:text-base md:text-lg ${
+          currentPage === totalPages
+            ? 'cursor-not-allowed opacity-50'
+            : 'hover:underline'
         }`}
         onClick={() =>
           currentPage < totalPages && onPageChange(currentPage + 1)
