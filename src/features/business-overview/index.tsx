fb33c609f2ea9cb30b6d756d2924e17810a2625c
JangHwan <wkd5878@gmail.com>
2025-02-16 20:44:43 +0900

    :sparkle: feat: Breadcrumb 컴포넌트 추가 및 경로 출력 기능 추가
    - 현재 경로를 동적으로 표시하여 '홈 > 사업개요 > 친환경 무 폐수 나염' 형태로 출력
    - useLocation() 을 활용하여 경로 기반 UI를 자동 업데이트

--- src/features/business-overview/index.tsx
+++ src/features/business-overview/index.tsx
@@ -1,9 +1,14 @@
-import PageLayout from '../../layout/PageLayout.tsx';
 import {Outlet} from 'react-router-dom';
+import PageLayout from '../../layout/PageLayout.tsx';
+import CurrentInfoPath from './components/CurrentInfoPath.tsx';
 
 const Business = () => {
   return (
-    <PageLayout title="사업개요 페이지 본문">
+    <PageLayout
+      title="사업개요 페이지 본문"
+      fullHeight={false}
+      minHeight={false}>
+      <CurrentInfoPath/>
       <Outlet />
     </PageLayout>
   );
--- src/layout/PageLayout.tsx
+++ src/layout/PageLayout.tsx
@@ -1,14 +1,27 @@
 import React from 'react';
+import clsx from 'clsx';
 
-const PageLayout = ({
-  title,
-  children,
-}: {
+interface PageLayoutProps {
   title: string;
   children: React.ReactNode;
-}) => {
+  fullHeight?: boolean;
+  minHeight?: boolean;
+}
+
+const PageLayout: React.FC<PageLayoutProps> = (props) => {
+  const {
+    title,
+    children,
+    fullHeight = true,
+    minHeight = true,
+  } = props;
+  const classes = clsx(
+    fullHeight ? "h-screen" : "h-auto",
+    minHeight && "min-h-screen",
+    "w-full mt-[calc(4rem+10px)] flex flex-col");
+
   return (
-    <main className={`mt-[calc(4rem+10px)] flex min-h-screen w-full flex-col`}>
+    <main className={classes}>
       {/* SEO 최적화용 */}
       <h1 className="hidden">{title}</h1>
       {children}
