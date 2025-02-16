6d66f6e3f22673afb46e91c929e59bb09bc483c1
JangHwan <wkd5878@gmail.com>
2025-02-17 01:44:57 +0900

    :art: design: News 상세페이지 컴포넌트 디자인 수정

--- src/features/news/pages/NewsDetail.tsx
+++ src/features/news/pages/NewsDetail.tsx
@@ -21,27 +21,28 @@ const NewsDetail: React.FC = () => {
   }
 
   return (
-    <section className="mx-auto max-w-screen-lg px-4 sm:px-6 md:px-8 py-8 sm:py-12">
-      <h1 className="mb-6 sm:mb-8 text-2xl sm:text-3xl md:text-4xl font-bold text-gray-900">
-        {newsItem.title}
-      </h1>
-      <div className="mb-4 sm:mb-6 text-xs sm:text-sm text-gray-500">
-        <span>{newsItem.author}</span> |{' '}
-        <span className="ml-1 sm:ml-2">
+    <section className="py-32 mx-auto max-w-screen-lg">
+      <div className="max-w-2xl mx-auto text-center">
+        <h1 className="mb-6 sm:mb-8 text-2xl sm:text-3xl md:text-4xl font-bold text-gray-900">
+          {newsItem.title}
+        </h1>
+        <div className="mb-4 sm:mb-6 text-sm sm:text-lg text-gray-500">
+          <span>{newsItem.author}</span> |{' '}
+          <span className="ml-1 sm:ml-2">
           {new Date(newsItem.publishedDate).toLocaleDateString()}
         </span>
+        </div>
       </div>
-      <hr className="mb-6 sm:mb-10 border-b-2 border-gray-300" />
+
       <img
         src={newsItem.imageUrl}
         alt={newsItem.title}
-        className="mt-4 sm:mt-8 mb-6 sm:mb-10 w-full h-auto object-contain"
+        className="mt-4 sm:mt-8 mb-6 sm:mb-10 w-screen h-96 object-contain"
       />
 
-      <article className="mt-6 text-base sm:text-lg md:text-xl leading-relaxed whitespace-pre-line text-gray-700">
+      <article className="max-w-3xl mx-auto mt-20 text-base sm:text-lg md:text-xl leading-relaxed whitespace-pre-line text-gray-700">
         {newsItem.content}
       </article>
-      <hr className="mt-8 sm:mt-12 border-b-2 border-gray-300" />
     </section>
   );
 };
