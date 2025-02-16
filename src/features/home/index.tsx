388ba5582fc58ddc19f1d761e485698df354af4b
JangHwan <wkd5878@gmail.com>
2025-02-16 19:22:44 +0900

    ♻️ refactor: Home 뉴스 섹션 높이 조절

--- src/features/home/index.tsx
+++ src/features/home/index.tsx
@@ -11,17 +11,17 @@ import newsData from '../../data/newsData.json';
 const Home = () => {
   const combinedNews = [...(newsData.news || []), ...(newsData.newsList || [])];
   const filteredNews = combinedNews.filter((news) =>
-    ['1', '2', '3', '4'].includes(news.id)
+    ['1', '2', '3', '4'].includes(news.id),
   );
   return (
     <PageLayout title="서비스 페이지 본문">
       <HeroSection
         sectionClass="h-[410px] md:h-[635px] grid grid-cols-1 grid-rows-1"
-        src={imageSrc}/>
+        src={imageSrc} />
       {/*<SectionAbout_Design1/>*/}
-      <SectionAbout_Design2/>
-        {/* 제품 */}
-        {/*<section className="mx-auto max-w-4xl">
+      <SectionAbout_Design2 />
+      {/* 제품 */}
+      {/*<section className="mx-auto max-w-4xl">
           <ProductList
             title="고객과 함께 하는 친환경 녹색 미래"
             description="비젼라이프는 지속 가능한 미래를 위해 친환경 기술을 연구하며, 혁신적인 솔루션으로 녹색 산업을 선도합니다."
@@ -34,9 +34,8 @@ const Home = () => {
           />
         </section>*/}
 
-        {/* 뉴스 */}
-      {/* 뉴스 섹션 */}
-      <section className="pt-20 mx-32 h-96 py-20">
+      {/* 뉴스 */}
+      <section className="pt-20 mx-32 h-auto py-20">
         <h2 className="mb-3 ml-3 flex items-center gap-3 text-lg font-semibold">
           <AiOutlineArrowDown className="text-primary font-extrabold" />
           비전라이프 최신 소식
@@ -50,9 +49,9 @@ const Home = () => {
         </ul>
       </section>
 
-        <section className="mx-auto h-96 max-w-3xl py-20">
-          <MoreNewsSection />
-        </section>
+      <section className="mx-auto h-auto max-w-3xl py-20">
+        <MoreNewsSection />
+      </section>
     </PageLayout>
   );
 };
