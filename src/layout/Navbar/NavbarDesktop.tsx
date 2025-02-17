a3f62ebfebec8e30a8a5361b6810380f31b2312b
JangHwan <wkd5878@gmail.com>
2025-02-17 13:15:32 +0900

    :bug: fix: 내비게이션 배경색이 적용되지 않는 문제 수정

--- src/layout/Navbar/NavbarDesktop.tsx
+++ src/layout/Navbar/NavbarDesktop.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Logo from '../../components/Logo.tsx';
 import {NavigationT} from './index.tsx';
 import NavbarMenu from './NavbarMenu.tsx';
+import clsx from 'clsx';
 
 interface DesktopNavbarProps {
   isDropdownVisible: boolean;
@@ -15,18 +16,25 @@ const NavbarDesktop: React.FC<DesktopNavbarProps> = (props) => {
     setIsDropdownVisible,
     navigation,
   } = props;
+
+  const subClasses = clsx(
+    'absolute inset-x-0 top-full border-t bg-white border-gray-200 shadow-md transition-all duration-300 ease-in-out',
+    isDropdownVisible ? 'opacity-100 visible' : 'opacity-0 invisible');
+
   return (
     <div
       className="hidden md:block relative"
       onMouseEnter={() => setIsDropdownVisible(true)}
       onMouseLeave={() => setIsDropdownVisible(false)}>
       <div className="mx-auto max-w-7xl flex items-center justify-between py-4 h-18">
-        <Logo type="_desktop"/>
-        <NavbarMenu type="_nav" items={navigation}/>
+        <Logo type="_desktop" />
+        <NavbarMenu type="_nav" items={navigation} />
       </div>
 
-      <div className={`mx-auto max-w-7xl flex absolute inset-x-0 top-full bg-white border-t border-gray-200 shadow-md transition-all duration-300 ease-in-out mt-${isDropdownVisible ? "opacity-100 visible" : "opacity-0 invisible"}`}>
-        <NavbarMenu type="_sub" items={navigation}/>
+      <div className={subClasses}>
+        <div className="w-full max-w-7xl mx-auto flex">
+          <NavbarMenu type="_sub" items={navigation} />
+        </div>
       </div>
     </div>
   );
