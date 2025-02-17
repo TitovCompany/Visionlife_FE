f173f4ec56e39aec40258d5629aa5707afa1bed3
JangHwan <wkd5878@gmail.com>
2025-02-17 12:55:55 +0900

    :bug: fix: Tailwind 스타일 적용되지 않는 문제 해결 (CSP 수정)

--- vite.config.ts
+++ vite.config.ts
@@ -7,10 +7,8 @@ export default defineConfig({
   // 커스텀 도메인사용시 base 빈값으로 설정
   base: '',
 
-  // 전역 상수로 대체되는 값을 정의(환경 변수)
-  define: {
-    'process.env': process.env,
-  },
+  // 전역 상수로 대체되는 값을 정의
+  define: {},
 
   // @를 src 디렉토리로 매핑
   resolve: {
@@ -30,8 +28,16 @@ export default defineConfig({
   },
 
   server: {
-    port: 3000, // 개발 서버 포트 지정
-    open: true, // 서버 실행 시 브라우저 자동 열기
+    // port: 3000,  // 개발 서버 포트 지정
+    // open: true,  // 서버 실행 시 브라우저 자동 열기
+
+    headers: {
+      // XSS 및 Clickjacking 방지
+      "content_security_policy": "default-src 'self' style-src 'self' 'unsafe-inline';",
+      'X-Frame-Options': 'DENY',  // Clickjacking 방지
+      'X-Content-Type-Options': 'nosniff',  // MIME 타입 스니핑 방지
+      'Referrer-Policy': 'strict-origin-when-cross-origin', // Referrer 정보 최소화
+    },
   },
 
   css: {
@@ -49,12 +55,14 @@ export default defineConfig({
         drop_debugger: true,  // debugger 제거
       }
     },
+
     // 소스맵 제거 (개발시 사용)
     sourcemap: false,
 
     // 캐싱, 최적화
     assetsDir: 'assets',
     rollupOptions: {
+      external: ['config.json'], // 번들에 포함되지 않도록 설정
       output: {
         entryFileNames: 'assets/[name].[hash].js',
         chunkFileNames: 'assets/[name].[hash].js',
@@ -68,6 +76,7 @@ export default defineConfig({
 
   optimizeDeps: {
     exclude: ['moment', 'lodash'], // 번들에서 제외할 패키지
+    include: ['react', 'react-dom'], // 최적화 포함 대상 지정
   },
 
   plugins: [
