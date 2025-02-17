0983c76002dcd7568f48b2cfca794dd0261551bd
JangHwan <wkd5878@gmail.com>
2025-02-17 19:04:03 +0900

    :recycle: refactor - news 타입 분리

---
+++ src/types/news.d.ts
@@ -0,0 +1,10 @@
+export interface NewsType {
+  id: string;
+  type: string;
+  imageUrl: string;
+  title: string;
+  content: string;
+  author: string;
+  publishedDate: string;
+  views: number;
+}
