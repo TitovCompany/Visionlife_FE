4321cb035e19545dbdec993c9652a0206b82c375
ko <kws07042@gmail.com>
2025-02-19 17:14:05 +0900

    feat- 페이지네이션 컴포넌트 동적 블록 크기 지원 추가
    pagesPerBlock prop을 추가하여 한 번에 표시되는 페이지 수를 동적으로 설정할 수 있도록 수정함
    
    기본값은 5로 설정되어 별도의 값 전달 없이 기본 동작 유지됨
    <Pagination
              currentPage={currentPage}
              totalPages={totalPages}
              onPageChange={setCurrentPage}
              pagesPerBlock={7}
            />
           넣으면 7까지페이 버튼 랜더링

--- src/components/Pagination.tsx
+++ src/components/Pagination.tsx
@@ -1,25 +1,29 @@
-// Pagination.tsx
 import React from 'react';
 
 interface PaginationProps {
   currentPage: number;
   totalPages: number;
   onPageChange: (page: number) => void;
+  pagesPerBlock?: number; // 페이지네이션 크기를 동적으로 설정할 수 있는 prop
 }
 
 const Pagination: React.FC<PaginationProps> = ({
                                                  currentPage,
                                                  totalPages,
                                                  onPageChange,
+                                                 pagesPerBlock = 5, // 기본 크기는 5
                                                }) => {
+  if (totalPages <= 1) return null;
+
+  const currentBlock = Math.ceil(currentPage / pagesPerBlock);
+  const startPage = (currentBlock - 1) * pagesPerBlock + 1;
+  const endPage = Math.min(startPage + pagesPerBlock - 1, totalPages);
 
   return (
     <div className="mt-16 flex flex-wrap justify-center items-center gap-2 sm:gap-4">
       <button
         className={`px-4 sm:px-8 py-1 text-base sm:text-lg ${
-          currentPage === 1
-            ? 'cursor-not-allowed opacity-50'
-            : 'cursor-pointer hover:underline'
+          currentPage === 1 ? 'cursor-not-allowed opacity-50' : 'cursor-pointer hover:underline'
         }`}
         onClick={() => currentPage > 1 && onPageChange(currentPage - 1)}
         disabled={currentPage === 1}
@@ -27,8 +31,8 @@ const Pagination: React.FC<PaginationProps> = ({
         &lt;
       </button>
 
-      {Array.from({ length: totalPages }, (_, index) => {
-        const pageNumber = index + 1;
+      {Array.from({ length: endPage - startPage + 1 }, (_, index) => {
+        const pageNumber = startPage + index;
         return (
           <button
             key={pageNumber}
@@ -46,13 +50,9 @@ const Pagination: React.FC<PaginationProps> = ({
 
       <button
         className={`px-4 sm:px-8 py-1 text-base sm:text-lg ${
-          currentPage === totalPages
-            ? 'cursor-not-allowed opacity-50'
-            : 'cursor-pointer hover:underline'
+          currentPage === totalPages ? 'cursor-not-allowed opacity-50' : 'cursor-pointer hover:underline'
         }`}
-        onClick={() =>
-          currentPage < totalPages && onPageChange(currentPage + 1)
-        }
+        onClick={() => currentPage < totalPages && onPageChange(currentPage + 1)}
         disabled={currentPage === totalPages}
       >
         &gt;
