3e5b777927d89a93c964878e7235836848573558
JangHwan <wkd5878@gmail.com>
2025-02-19 11:38:19 +0900

    :recycle: refactor - 사업 소개 히어로 디자인 변경

--- src/features/business-overview/pages/Overview.tsx
+++ src/features/business-overview/pages/Overview.tsx
@@ -5,35 +5,36 @@ import SectionLayout from "../../../layout/SectionLayout.tsx";
 
 const Overview = () => {
   return (
-    <div className="container mx-auto">
+    <div className="max-w-lg md:max-w-2xl lg:max-w-7xl mx-auto">
       <SectionLayout
         title="비전라이프 사업 소개"
         className="py-32 grid place-items-center"
-        titleClassName="hidden"
-        titlePadding="p-0">
+        titleClassName="hidden">
 
-        <div className="w-full pb-28">
-          <h2 className="flex w-full max-w-lg md:max-w-xl lg:max-w-2xl mx-auto items-center justify-center gap-2 md:gap-4 lg:gap-6 pb-10 text-3xl md:text-4xl lg:text-5xl font-extrabold">
-            <span className="h-[2px] w-10 md:w-16 bg-[#deee]"></span>
-            {overview.header}
-            <span className="h-[2px] w-10 md:w-16 bg-[#deee]"></span>
-          </h2>
+        <article className="mx-auto w-full md:text-xl leading-relaxed flex lg:flex-row gap-10">
+          <div className="w-full max-w-lg mr-auto">
+            <h2 className="w-full max-w-lg md:max-w-xl lg:max-w-2xl mx-auto pb-10 text-3xl md:text-4xl lg:text-5xl font-extrabold">
+              {overview.header}
+            </h2>
+            <h3 className="text-lg sm:text-xl md:text-2xl leading-normal font-semibold">
+              <span className="text-primary">비전라이프</span>의 친환경 에코 잉크와
+              <br />
+              무폐수 디지털 나염 시스템
+            </h3>
 
-          <h3 className="text-center text-lg sm:text-xl md:text-2xl leading-normal font-semibold">
-            비젼라이프의 친환경 에코 잉크와
-            <br />
-            무폐수 디지털 나염 시스템
-          </h3>
-        </div>
-        <div className="px-4 md:px-8">
-          <article className="mx-auto w-full max-w-lg md:max-w-2xl lg:max-w-4xl md:text-xl leading-relaxed">
-            <div className="border-l-4 border-primary pl-4 md:pl-6">
+            <div className="text-lg">
               {overview.description.map((line: string, index: number) => (
-                <p key={index} className="mt-3 md:mt-4">{line}</p>
+                <p key={index} className="mt-3 md:mt-4 leading-relaxed">{line}</p>
               ))}
             </div>
-          </article>
+          </div>
+          <img
+            src="/img/business/p1.jpg"
+            alt=""
+            className="w-96 h-96 lg:h-[500px] object-cover"/>
+        </article>
 
+        <div className="px-4 md:px-8">
           <div className="w-full pt-32 space-y-12 md:space-y-10 lg:space-y-12">
             <OverviewList data={overview.features} />
             <div className="pt-20">
