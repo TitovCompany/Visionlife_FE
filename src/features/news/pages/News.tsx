cc26b1079f39f4f47e083953833a2a99ccd1ed97
ko <kws07042@gmail.com>
2025-02-19 14:38:05 +0900

    feat: 뉴스 페이지 레이아웃 분리 및 모바일 지원
    - 데스크탑에서 큰 뉴스 3개 와 나머지 뉴스 리스트를 별도 영역에 렌더링하도록 분리
    - 모바일에서는 MobileVerticalNewsList 컴포넌트를 사용해 세로 뉴스 리스트를 표시하도록 개선

--- src/features/news/pages/News.tsx
+++ src/features/news/pages/News.tsx
@@ -1,4 +1,5 @@
 import NewsCardList from '../components/NewsCardList.tsx';
+import MobileVerticalNewsList from '../components/MobileVerticalNewsList.tsx';
 
 const News = () => {
   return (
@@ -7,8 +8,21 @@ const News = () => {
         <h2 className="mb-8 text-5xl text-gray-900">언론 보도</h2>
         <h3 className="text-2xl">비전라이프 최신 뉴스</h3>
       </div>
-      <NewsCardList layout="horizontal"/>
-      <NewsCardList layout="vertical"/>
+
+      {/* 데스크탑용 큰 뉴스 3개*/}
+      <div className="hidden sm:block">
+        <NewsCardList layout="horizontal" />
+      </div>
+
+      {/* 데스크탑용 나머지 뉴스 리스트*/}
+      <div className="hidden sm:block mt-8">
+        <NewsCardList layout="vertical" />
+      </div>
+
+      {/* 모바일 전용 세로 레이아웃 */}
+      <div className="block sm:hidden">
+        <MobileVerticalNewsList />
+      </div>
     </section>
   );
 };
