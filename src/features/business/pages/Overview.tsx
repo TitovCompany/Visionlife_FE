ef43d44263d904b5b5c8ba9d8e39a94ec1388a76
JangHwan <wkd5878@gmail.com>
2025-02-21 02:49:19 +0900

    ♻️ refactor: Business Overview 반응형 작업
    - clsx를 활용하여 클래스명을 변수화하여 가독성 및 유지보수성 향상
    - isReversed 속성을 활용하여 이미지와 텍스트의 배치 유연성 개선
    - 반응형 디자인 적용 (lg 이상에서 2열 레이아웃 유지)
    - 이미지 및 텍스트 컨테이너의 클래스 구조 정리 (CL: 473)

--- src/features/business/pages/Overview.tsx
+++ src/features/business/pages/Overview.tsx
@@ -8,38 +8,31 @@ import {useRef} from 'react';
 
 const Overview = () => {
   const sectionRef = useRef(null);
-  useScrollAnimation('.biz_item', 'top 80%')
+  useScrollAnimation('.biz_item', 'top 80%');
+
   const maxWidth = 'max-w-lg md:max-w-2xl lg:max-w-5xl mx-auto';
   return (
     <SectionLayout
       title="비전라이프 사업 소개"
-      className="mx-auto grid w-full place-items-center md:py-32"
+      className="overflow-x-hidden mx-auto grid w-full place-items-center sm:py-16 md:py-32"
       titleClassName="hidden">
-      <article
-        className={`${maxWidth} flex w-full flex-col-reverse gap-10 leading-relaxed md:text-xl lg:flex-row`}>
-        <div className="mx-auto w-full max-w-sm md:mr-auto md:max-w-lg">
+      <article className={`${maxWidth} flex w-full flex-col-reverse gap-10 leading-relaxed md:text-xl lg:flex-row`}>
+        <div className="mx-auto w-full max-w-xs sm:max-w-sm md:mr-auto md:max-w-lg">
           <h2 className="md:text-4x pb-5 text-3xl font-extrabold md:pb-10 lg:text-5xl">
             {overview.header}
           </h2>
           <h3 className="text-xl leading-normal font-semibold md:text-2xl">
             <span className="text-primary">비전라이프</span>의 친환경 에코
-            잉크와
-            <br />
-            무폐수 디지털 나염 시스템
+            잉크와<br/>무폐수 디지털 나염 시스템
           </h3>
 
           <div className="text-md md:text-lg">
             {overview.description.map((line: string, index: number) => (
-              <p ref={sectionRef} key={index} className="biz_item mt-3 leading-relaxed md:mt-4">
-                {line}
-              </p>
+              <p ref={sectionRef} key={index} className="biz_item mt-3 leading-relaxed md:mt-4">{line}</p>
             ))}
           </div>
         </div>
-        <img
-          src="/img/business/p1.jpg"
-          alt=""
-          className="h-96 w-full object-cover md:h-[500px] md:w-[50%]" />
+        <img src="/img/business/p1.jpg" alt="" className="h-96 w-full object-cover md:h-[500px] sm:w-full lg:w-[50%]" />
       </article>
 
       {/* 4가지 장점 */}
@@ -53,8 +46,8 @@ const Overview = () => {
         />
       ))}
 
-      <article className="mx-auto w-full max-w-sm pb-20 lg:max-w-3xl">
-        <h2 className="pt-32 text-2xl leading-relaxed font-bold lg:text-4xl">
+      <article className="mx-auto w-full max-w-xs sm:max-w-sm pb-20 lg:max-w-3xl">
+        <h2 className="pt-16 md:pt-32 text-2xl leading-relaxed font-bold lg:text-4xl">
           디지털 나염으로
           <br />
           완성하는 <span className="text-primary">친환경 생산</span>
