595e965a921c12056adc769546d36d94971d0d80
ko <kws07042@gmail.com>
2025-02-21 01:43:15 +0900

    refactor: 자료실 페이지 레이아웃 반응형 개선
    - xs:max-w-xs 및 px-2 추가하여 작은 화면에서의 가독성과 여백 조정
    - `lg:max-w-5xl` → `lg:max-w-4xl`, `xl:max-w-7xl` → `xl:max-w-6xl`로 변경하여 레이아웃 균형 조정
    - grid-cols-1을 유지하여 모든 해상도에서 1 열 구조 적용
    - lg:flex lg:flex-col lg:gap-10을 제거하여 일관된 리스트 형식 유지
    
    - 테스트 디바이스
    
    - (ipadmini)(ipadair)(ipadpro)
    (Z Fold2,3,4,5)(Z Flip 3,4,5)
    (iphone16,plus,ProMax)

--- src/features/catalog/index.tsx
+++ src/features/catalog/index.tsx
@@ -18,18 +18,18 @@ const Catalog = () => {
 
   return (
     <PageLayout title="카탈로그" isFullHeight={false} isMinHeight={false}>
-      <div className="mx-auto min-h-full w-full max-w-sm flex-grow md:max-w-2xl lg:max-w-5xl xl:max-w-7xl">
+      <div className="mx-auto min-h-full w-full xs:max-w-xs px-2 sm:max-w-sm md:max-w-2xl lg:max-w-4xl xl:max-w-6xl ">
         <div className="py-12 md:py-16 lg:py-20">
           <h1 className="border-b-primary border-b pb-8 text-left text-4xl font-bold text-gray-900">
             자료실
           </h1>
 
-          <ul className="mt-6 grid grid-cols-1 gap-5 sm:grid-cols-1 md:grid-cols-2 lg:flex lg:flex-col lg:gap-10">
+          {/* 모든 해상도에서 1열 유지 */}
+          <ul className="mt-6 grid grid-cols-1 gap-5">
             {catalogData.map((item: CatalogItem) => (
               <DownloadItem key={item.id} data={item} />
             ))}
           </ul>
-
           {/*<div className="mt-8 mb-8">
           <Pagination
             currentPage={currentPage}
