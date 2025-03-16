c97603daab934e5266604e449c2a8ba82b328696
japark <wkd5878@gmail.com>
2025-03-16 11:44:11 +0900

    :art: chore - 히어로 섹션 내 애니메이션 수정

--- src/features/home/components/Hero_v2.tsx
+++ src/features/home/components/Hero_v2.tsx
@@ -13,6 +13,9 @@ const buttonList = ['PROUTEX', 'Title 01', 'Title 02', 'Title 03'];
 
 const HeroV2 = () => {
  const [currentIndex, setCurrentIndex] = useState(0);
+ const titleRef = useRef(null);
+ const contentRef = useRef<(HTMLElement | null)[]>([]);
+ const imageRef = useRef(null);
  const sliderRef = useRef<HTMLElement[] | []>([]);
  const slideTimeRef = useRef<HTMLDivElement | null>(null);
 
@@ -21,31 +24,62 @@ const HeroV2 = () => {
    setCurrentIndex((prevIndex) =>
     prevIndex === carouselItems.length ? 0 : prevIndex + 1
    );
-  }, 5000);
+  }, 11115000);
 
   return () => clearInterval(interval); // 컴포넌트 언마운트 시 정리
  }, []);
 
  useGSAP(() => {
   if (sliderRef.current.length === 0) return;
+  const tl = gsap.timeline();
+
+  // 타이틀 애니메이션
+  tl.fromTo(imageRef.current, {
+   y: 100,
+   opacity: 0,
+  }, {
+   y: 0,
+   opacity: 1,
+   duration: 1.2,
+  })
+  tl.fromTo(titleRef.current, {
+   y: 100,
+   opacity: 0,
+  }, {
+   y: 0,
+   opacity: 1,
+   duration: 1.2,
+  })
+  tl.fromTo(contentRef.current, {
+   y: 100,
+   opacity: 0,
+  }, {
+   y: 0,
+   opacity: 1,
+   duration: 1.2,
+  })
 
   // 기존 애니메이션 정리
   gsap.killTweensOf(sliderRef.current);
   gsap.killTweensOf(slideTimeRef.current);
 
-    // 슬라이드 이동 애니메이션
-    gsap.to(sliderRef.current, {
-     xPercent: -100 * currentIndex,
-    })
-  
-    // 프로그래스 바 애니메이션 (5초 동안 증가)
-    gsap.fromTo(slideTimeRef.current, {
-     width: '0%'
-    },{
-     width: '100%',
-     duration: 5,
-     ease: "linear",
-    });
+  // 슬라이드 이동 애니메이션
+  gsap.to(sliderRef.current, {
+   xPercent: -100 * currentIndex,
+  });
+
+  // 프로그래스 바 애니메이션 (5초 동안 증가)
+  gsap.fromTo(
+   slideTimeRef.current,
+   {
+    width: '0%',
+   },
+   {
+    width: '100%',
+    duration: 5,
+    ease: 'linear',
+   }
+  );
  }, [currentIndex]);
 
  // 버튼 클릭시 슬라이드 변경
@@ -54,24 +88,21 @@ const HeroV2 = () => {
  };
 
  return (
-  <GridArticle className='relative bg-primary text-color'>
+  <GridArticle className='bg-primary text-color relative'>
    {/* Slider */}
    <div className='flex h-[768px] w-full whitespace-nowrap'>
-    <div
-     ref={(el) => {
-      if (el) sliderRef.current[0] = el;
-     }}
-     className='flex w-full flex-shrink-0 flex-col items-center justify-center pt-20 pb-40'>
-     <img
-      src='/img/logo.webp'
-      alt='히어로 섹션 이미지'
-      className='h-full w-[400px]'
-     />
-     <h2 className='mb-10 text-7xl'>PROUTEX</h2>
-     <p className='mb-5 text-4xl'>
-      (주)비젼라이프홀딩스의 나일론잉크 “N-RECT 엔렉”은
-     </p>
-     <p className='text-3xl'>99% 무폐수로 만들어집니다.</p>
+    <div ref={(el) => {
+      if (el) sliderRef.current[0] = el;}}
+      className='flex w-full flex-shrink-0 flex-col items-center justify-center pt-20 pb-40'>
+     {/* Title */}
+     <img ref={imageRef} src='/img/logo.webp' alt='히어로 섹션 이미지' className='h-full w-[400px]' />
+     <h2 ref={titleRef} className='mb-10 text-7xl'>PROUTEX</h2>
+     <p ref={(el) => {
+      if (el) contentRef.current[0] = el;
+     }} className='mb-5 text-4xl'>(주)비젼라이프홀딩스의 나일론잉크 “N-RECT 엔렉”은</p>
+     <p ref={(el) => {
+      if (el) contentRef.current[1] = el;
+     }} className='text-3xl'>99% 무폐수로 만들어집니다.</p>
     </div>
 
     {carouselItems.map((item, index) => (
@@ -84,10 +115,10 @@ const HeroV2 = () => {
       <img
        src={item.src}
        alt='히어로 섹션 이미지'
-       className='absolute h-[857px] z-[999] w-full bg-black/10'
+       className='absolute z-[999] h-[857px] w-full bg-black/10'
       />
-      <div className='absolute z-[9999] top-[40%] left-1/2 -translate-x-1/2'>
-       <h3 className='text-7xl text-color'>Title</h3>
+      <div className='absolute top-[40%] left-1/2 z-[9999] -translate-x-1/2'>
+       <h3 className='text-color text-7xl'>Title</h3>
        <p className='mt-5 text-4xl'>Description</p>
        <button className='mt-10 text-2xl'>Move</button>
       </div>
@@ -99,14 +130,14 @@ const HeroV2 = () => {
     <nav className='text-color w-full pt-5'>
      <ul className='mx-auto flex w-full max-w-xl justify-between'>
       {buttonList.map((item, index) => (
-        <li
-          key={index}
-          className={`cursor-pointer px-4 py-2 ${
-            currentIndex === index ? 'bg-white font-bold text-black' : ''
-          }`}
-          onClick={() => handleButtonClick(index)}>
-         {item}
-        </li>
+       <li
+        key={index}
+        className={`cursor-pointer px-4 py-2 ${
+         currentIndex === index ? 'bg-white font-bold text-black' : ''
+        }`}
+        onClick={() => handleButtonClick(index)}>
+        {item}
+       </li>
       ))}
      </ul>
     </nav>
