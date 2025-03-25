bcdcfff96e0a6d4041746155aef64e9579472777
JangHwan <wkd5878@gmail.com>
2025-03-25 18:10:17 +0900

    :art: chore - Company 내 이미지 마우스 오버시 확대 이벤트 추가

--- src/components/LinkList.tsx
+++ src/components/LinkList.tsx
@@ -10,13 +10,9 @@ interface LinkListProps {
 
 const LinkList = ({href, imgSrc, text, className}: LinkListProps) => {
  return (
-  <li className={clsx('relative h-96 w-full p-8', className)}>
+  <li className={clsx('group relative h-96 w-full p-8 overflow-hidden transition-transform duration-500 ease-in-out hover:scale-105', className)}>
    <Link to={href}>
-    <img
-     src={imgSrc}
-     alt={text}
-     className='absolute top-0 left-0 h-full w-full'
-    />
+    <img src={imgSrc} alt={text} className="absolute top-0 left-0 h-full w-full object-cover transition-transform duration-700 ease-in-out group-hover:scale-110"/>
     <p className='absolute top-3/4 left-1/2 -translate-1/2 text-2xl font-bold text-white'>
      {text}
     </p>
