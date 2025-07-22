cc2593decdc7e34eefb018a6d2fc2969adb8ed41
JangHwan <wkd5878@gmail.com>
2025-07-22 13:39:28 +0900

    :art: design - business 페이지내 데이터 분리 및 불필요한 로직 주석처리

--- src/features/brand_identity/index.tsx
+++
@@ -1,9 +0,0 @@
-const BrandIdentity = () => {
- return (
-  <div>
-
-  </div>
- );
-};
-
-export default BrandIdentity;
--- src/features/product/business/index.tsx
+++ src/features/product/business/index.tsx
@@ -8,78 +8,9 @@ import { useGSAP } from '@gsap/react';
 import overview from '../../../data/business/overview.json';
 import clsx from 'clsx';
 import { Button } from 'japark-react-components';
+import {contentSections, sectionLinks} from '../../../data/business/business.ts';
 
 gsap.registerPlugin(ScrollTrigger);
-
-const sectionLinks = [
- { to: '/', label: 'ECO-FRIENDLY INNOVATION' },
- { to: '/', label: 'INK TECHNOLOGY' },
- { to: '/', label: 'SUSTAINABLE MANUFACTURING' },
- { to: '/', label: 'PARTNERSHIP & GROWTH' }
-];
-
-const contentSections = [
- [
-  {
-   number: '01',
-   title: '지속 가능한 친환경 기술',
-   desc:
-    '비전라이프는 무폐수 디지털 나염을 통해\n80% 물 절감 & 탄소 배출 50% 감소를 실현합니다. 친환경 공정으로 지속 가능한 섬유 산업을 선도합니다.'
-  },
-  {
-   number: '02',
-   title: '친환경 인증 & 글로벌 기준 준수',
-   desc:
-    'OEKO-TEX®, GOTS 등 국제 친환경 인증을 획득하여, 글로벌 시장에서도 신뢰할 수 있는 품질을 제공합니다.'
-  },
-  { number: '03', title: 'Title', desc: 'Description' }
- ],
- [
-  {
-   number: '01',
-   title: '고품질 & 내구성 강화된 잉크',
-   desc:
-    'N-RECT 친환경 수성 잉크는 기존 잉크보다 더 선명한 색감과 뛰어난 내구성을 제공합니다.'
-  },
-  {
-   number: '02',
-   title: '빠른 흡수력 & 최적의 컬러 품질',
-   desc:
-    '최적의 흡수 속도로 번짐 없이 섬유 표면 깊숙이 스며들어 고품질의 프린팅을 구현합니다.'
-  },
-  { number: '03', title: 'Title', desc: 'Description' }
- ],
- [
-  {
-   number: '01',
-   title: '공정 간소화 & 비용 절감',
-   desc:
-    '전처리 & 후처리 과정이 필요 없는 효율적인 디지털 나염 공정으로 생산 비용과 에너지를 절감할 수 있습니다.'
-  },
-  {
-   number: '02',
-   title: '다양한 원단에 적용 가능',
-   desc:
-    '면, 폴리에스터, 나일론 등 다양한 원단에서도 일관된 품질을 유지합니다.'
-  },
-  { number: '03', title: 'Title', desc: 'Description' }
- ],
- [
-  {
-   number: '01',
-   title: '글로벌 브랜드와 협력',
-   desc:
-    '비전라이프는 B2B 파트너십을 통해 기업의 지속 가능한 성장과 브랜드 가치를 높이고 있습니다.'
-  },
-  {
-   number: '02',
-   title: '친환경 시장 진출 지원',
-   desc:
-    '친환경 패션 & 텍스타일 시장 진출을 원하는 기업에 맞춤형 솔루션을 제공합니다.'
-  },
-  { number: '03', title: 'Title', desc: 'Description' }
- ]
-];
 
 const Business = () => {
  const [activeIndex, setActiveIndex] = useState<number | null>(null);
@@ -149,7 +80,8 @@ const Business = () => {
       ))}
      </ul>
     </section>
-    <div className={clsx('absolute top-0 z-[999999]')}>
+    {/* ??? */}
+    {/*<div className={clsx('absolute top-0 z-[999999]')}>
      <ul>
       {sectionLinks.map((link, idx) => (
        <li key={idx}>
@@ -159,7 +91,7 @@ const Business = () => {
        </li>
       ))}
      </ul>
-    </div>
+    </div>*/}
     {activeIndex !== null && (
      <div
       ref={detailRef}
