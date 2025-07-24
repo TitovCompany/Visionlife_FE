c3f609f49197c4809aca7a8e4ddfc0c04fdd7fc6
JangHwan <wkd5878@gmail.com>
2025-07-24 17:47:43 +0900

    :art: design - 히어로 섹션 워딩 가운데 위치하도록 수정

--- src/features/home/components/HeroSlider.tsx
+++ src/features/home/components/HeroSlider.tsx
@@ -53,7 +53,7 @@ const HeroSlider = memo(({
      }}>
      {/* Image */}
      <img ref={imageRef} src='/logo.webp' alt='히어로 섹션 이미지' className='h-full w-full max-w-2/3 md:w-[600px]' />
-     <div className='md:-translate-y-16 mb-5 text-xl md:text-4xl leading-12'>
+     <div className='md:-translate-y-16 mb-5 text-xl md:text-4xl leading-8'>
       {['(주)비전라이프홀딩스의 나일론잉크', '“N-RECT 엔렉”은 99% 무폐수로 만들어집니다.'].map((item, index) => (
        <p key={index} ref={(el) => { if (el) contentRef.current[index] = el;}}>{item}</p>
       ))}
@@ -65,7 +65,7 @@ const HeroSlider = memo(({
       <video src={item.src} controls={false} className='h-[calc(100vh-68px)] w-screen object-cover brightness-70 filter' autoPlay={index === currentIndex} loop muted playsInline />
       <div className='w-screen max-w-10/12 absolute top-[35%] md:top-[40%] left-1/2 z-[9999] -translate-x-1/2'>
        <h3 className='mb-4 md:mb-10 text-4xl md:text-6xl font-bold'>{item.title}</h3>
-       <p className='mt-5 max-w-full md:max-w-3xl md:text-xl leading-8 whitespace-pre-line'>
+       <p className='mt-5 max-w-full md:max-w-3xl md:mx-auto md:text-xl leading-8 whitespace-pre-line'>
         {item.description}
        </p>
       </div>
